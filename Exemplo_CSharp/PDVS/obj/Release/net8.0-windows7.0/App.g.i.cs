ce8954189b54665c4a9254a520079dbacf99c467
Emanoel Fortuna <emanoellfortuna@gmail.com>
2025-02-21 14:01:33 -0300

    Atualização

--- Exemplo_CSharp/PDVS/obj/Release/net8.0-windows7.0/App.g.i.cs
+++ Exemplo_CSharp/PDVS/obj/Release/net8.0-windows7.0/App.g.i.cs
@@ -46,7 +46,7 @@ namespace PDVS {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -61,7 +61,7 @@ namespace PDVS {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public static void Main() {
             PDVS.App app = new PDVS.App();
             app.InitializeComponent();
